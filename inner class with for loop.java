af65a2503d79e85c63722cd8b7bff6d1d13cd583
Istiyak Ahmad <116272714+Istiyakahmad@users.noreply.github.com>
2022-12-06 21:56:53 +0530

    Add files via upload

---
+++ inner class with for loop.java
@@ -0,0 +1,21 @@
+Class Outer {
+int outer_x = 100;
+void test() {
+for(int i=10; i<10; i++) {
+class Inner {
+void display(){
+System.out.println("display: outer_x = outer_x);
+}
+}
+Inner inner = new Inner();
+inner.display();
+}
+}
+}
+class InnerClassDemo{
+public static void main (String args[]){
+Outer outer = new Outer();
+outer.test();
+}
+}
+ 
