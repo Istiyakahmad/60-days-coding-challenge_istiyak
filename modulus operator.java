7179efe8094c9435a539f5e84e22ccec7aa42e48
Istiyak Ahmad <116272714+Istiyakahmad@users.noreply.github.com>
2022-12-10 12:19:34 +0530

    Add files via upload

---
+++ modulus operator.java
@@ -0,0 +1,10 @@
+package batch1;
+public class Modulusoperator {
+private static final int Y = 0;
+public static void main(String args[]) {
+	int x = 42;
+	double y = 42.25;
+	System.out.println("x mod 10 =" + x % 10);
+	System.out.println("y mod 10 =" + Y % 10);
+}
+}
