c4f24dd39025e566b4d0018e52dde4fda7d1c605
Istiyak Ahmad <116272714+Istiyakahmad@users.noreply.github.com>
2022-12-12 12:07:53 +0530

    Add files via upload

---
+++ Stringconversion.java
@@ -0,0 +1,8 @@
+public class Stringconversion {
+public static void main(String[] args) {
+int num = 19648;
+System.out.println(num + "in binary:"+ Integer.toBinaryString(num));
+System.out.println(num + "in octal:"+ Integer.toOctalString(num));
+System.out.println(num + "in Hexadecimal : "+ Integer.toBinaryString(num));
+}
+	
