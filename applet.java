2acd31625ef4535ae5cd1b0dbb3d73c98efe084a
Istiyak Ahmad <116272714+Istiyakahmad@users.noreply.github.com>
2022-12-18 19:40:55 +0530

    Add files via upload

---
+++ applet.java
@@ -0,0 +1,12 @@
+package batch1;
+import java.awt.*;
+import java.applet.*;
+/*
+ <applet code="SimpleApplet" width=200 height=60>
+ </applet>
+ */
+public class SimpleApplet extends applet {
+	public void paint(Graphics g) {
+		g.drawString("A Simple Applet", 20,20);
+	}
+}
